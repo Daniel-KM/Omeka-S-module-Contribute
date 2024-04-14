98ec344f756bac97ce30ed3a9b95d84fcf422dce
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-04-15 00:00:00 +0200

    Updated Common version.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -25,10 +25,10 @@ $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
 // $entityManager = $services->get('Omeka\EntityManager');
 
-if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.55')) {
+if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.56')) {
     $message = new Message(
         'The module %1$s should be upgraded to version %2$s or later.', // @translate
-        'Common', '3.4.55'
+        'Common', '3.4.56'
     );
     throw new ModuleCannotInstallException((string) $message);
 }
